55d1b68b8237dcfd9b441edda69e4e897b993b70
Seyoon <seyoonleee@gmail.com>
2023-02-15 13:11:08 +0900

    튜토리얼 완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import { QueryClientProvider, QueryClient, useQuery } from 'react-query';
+import axios from 'axios';
+
+const queryClient = new QueryClient(); // queryClient 생성
 
 const Main = () => {
-  return <h1>Hello World</h1>;
+  console.log('Main Render!');
+  return (
+    // Gallery에 QueryClient 제공
+    <QueryClientProvider client={queryClient}>
+      {/* {console.log('queryClient', queryClient)} */}
+      <Gallery />
+    </QueryClientProvider>
+  );
+};
+
+const Gallery = () => {
+  console.log('Gallery Render!');
+  // useQuery 함수는 data, error, isLoading ....등의 키값을 가진 객체를 반환한다.
+  // console.log('useQuery()', useQuery());
+
+  // useQuery함수의 반환값에서 필요한 데이터 구조분해할당으로 초기화
+  // const { isLoading, error, data }
+  const res = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () => {
+      return axios(
+        'https://api.github.com/repos/tannerlinsley/react-query'
+      ).then(res => res.data);
+      // return fetch(
+      //   'https://api.github.com/repos/tannerlinsley/react-query'
+      // ).then(res => res.json());
+    },
+  });
+  // console.log('isLoading', isLoading);
+  // console.log('error', error);
+  // console.log('data', data);
+  // console.log('res', res);
+  console.log('res.isLoading', res.isLoading);
+  if (res.isLoading) return 'Loading...';
+
+  console.log('res.error', res.error);
+  if (res.error) return 'An error has occured: ' + res.error.message;
+
+  console.log('res.data', res.data);
+
+  const { data } = res;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  );
 };
 
 export default Main;
